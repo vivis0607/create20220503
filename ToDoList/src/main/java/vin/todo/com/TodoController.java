2c41cde96e676bec430b574f4e21aaa24bd3ae4a
vivis0607 <100543171+vivis0607@users.noreply.github.com>
2022-05-22 21:13:38 +0900

    0522
    todo프로젝트 환경설정중

---
+++ ToDoList/src/main/java/vin/todo/com/TodoController.java
@@ -0,0 +1,15 @@
+package vin.todo.com;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todo")
+public class TodoController {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "todo/home";
+	}
+}
